e0af06d3002f54b7d750b9080ffe08bfd75bcb66
Byte Cats <devin@bytecats.codes>
2023-05-15 02:57:18 -0400

    logging.go verbose commentary

--- logging/logging.go
+++ logging/logging.go
@@ -10,6 +10,10 @@ import (
 	"github.com/tidwall/pretty"
 )
 
+// LoggingMiddleware is a middleware function that logs the request and response details.
+// It logs the request method, URL, body, and the response status code and body.
+// The body of the request and response is pretty-printed for better readability.
+// If the body is too long, it is truncated for brevity in the logs.
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Log the request information
@@ -59,17 +63,21 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+// statusRecorder is a custom response writer that records the status code and body of the response.
+// It embeds http.ResponseWriter and adds fields for the status code and body.
 type statusRecorder struct {
 	http.ResponseWriter
 	statusCode int
 	body       bytes.Buffer
 }
 
+// WriteHeader sets the status code for the HTTP response and records it.
 func (r *statusRecorder) WriteHeader(statusCode int) {
 	r.statusCode = statusCode
 	r.ResponseWriter.WriteHeader(statusCode)
 }
 
+// Write writes the data to the connection as part of an HTTP reply and records the body.
 func (r *statusRecorder) Write(body []byte) (int, error) {
 	r.body.Write(body)
 	return r.ResponseWriter.Write(body)
